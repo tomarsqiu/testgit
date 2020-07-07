ac6faf95c54aeed193ee70f33e43857bacbea472
tomarsqiu <37662239+tomarsqiu@users.noreply.github.com>
2020-07-08 05:32:58 +0800

    Update HelloGit.java

--- gitweb/src/main/java/HelloGit.java
+++ gitweb/src/main/java/HelloGit.java
@@ -6,7 +6,7 @@ public class HelloGit {
         System.out.println ("主干添加");
         System.out.println ("分支添加");
         System.out.println ("主干添加2");
-        System.out.println ("创建分支2");
+        System.out.println ("创建分支2222");
 
     }
 }
